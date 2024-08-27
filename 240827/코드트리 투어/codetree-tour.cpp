a3a345e791c7868ede9023db001d81481a489450
오정한 <129676250+JeongHanO@users.noreply.github.com>
2024-08-27 17:13:21 +0900

    Title: 코드트리 투어, Time: 185ms, Memory: 1MB, Status: Passed - CodeTree

--- 240827/코드트리 투어/codetree-tour.cpp
+++ 240827/코드트리 투어/codetree-tour.cpp
@@ -1,122 +1,118 @@
 #include <iostream>
-#include <queue>
 #include <vector>
 #include <unordered_map>
+#include <queue>
 using namespace std;
 
-#define creat_land 100
-#define creat_product 200
-#define cancel_product 300
-#define opti_sales 400
-#define change_start 500
-
-struct Edge {
-    int num;
-    int cost;
-    bool operator < (Edge right) const {
-        return cost > right.cost;
-    }
+struct NODE {
+	int revenue;
+	int destination;
+	int distance;
 };
 
-struct product {
-    int revenue;
-    int* cost;
+struct cmp {
+	bool operator()(pair<int, int> a, pair<int, int> b) {
+		if (a.first < b.first) return true;
+		if (a.first == b.first) {
+			if (a.second > b.second) return true;
+		}
+		return false;
+	}
 };
 
-unordered_map<int, product> sale_product;
-vector<Edge> alis[10001];
-int n, m;
-int dis[2001];
-int pre_start_point = 0;
+int Q, n, m, s = 0;
+int land[2010];
+vector<pair<int, int>> graph[2010]; // 노드, 거리
+unordered_map<int, NODE> um; // id, NODE(매출, 도착지, 거리)
+priority_queue < pair<int, int>, vector<pair<int, int>>, cmp> pq; // 수익, id
 
-void dijkstra(int start) {
-    for (int i = 0; i < n; i++) {
-        dis[i] = 21e8;
-    }
-    priority_queue<Edge> pq;
-    pq.push({ start, 0 });
-    dis[start] = 0;
-
-    while (!pq.empty()) {
-        auto now = pq.top(); pq.pop();
-        if (dis[now.num] < now.cost) continue;
-
-        for (const Edge& next : alis[now.num]) {
-            int nextcost = dis[now.num] + next.cost;
-            if (nextcost < dis[next.num]) {
-                dis[next.num] = nextcost;
-                pq.push({ next.num, nextcost });
-            }
-        }
-    }
+void dij(int s) {
+	for (int i = 0; i < n; i++) {
+		land[i] = 1e8;
+	}
+	land[s] = 0;
+	priority_queue<pair<int, int>> q;
+	q.push({ 0, s }); // 거리, 노드
+	while (!q.empty()) {
+		int dist = -q.top().first;
+		int now = q.top().second;
+		q.pop();
+		if (land[now] < dist) continue;
+		for (int i = 0; i < graph[now].size(); i++) {
+			int nextDist = graph[now][i].second + land[now];
+			int next = graph[now][i].first;
+			if (nextDist < land[next]) {
+				land[next] = nextDist;
+				q.push({ -nextDist, next });
+			}
+		}
+	}
 }
 
 int main() {
+	cin >> Q;
+	int order;
+	cin >> order >> n >> m;
+	for (int i = 0; i < m; i++) {
+		int a, b, c;
+		cin >> a >> b >> c;
+		graph[a].push_back({ b, c });
+		graph[b].push_back({ a, c });
+	}
 
-    int t;
-    cin >> t;
+	dij(s);
+	Q--;
 
-    for (int test = 0; test < t; test++) {
-        int index;
-        cin >> index;
-
-        // 코드트리 랜드 건설
-        if (creat_land == index) {
-            cin >> n >> m;
-            for (int i = 0; i < m; i++) {
-                int from, to, cost;
-                cin >> from >> to >> cost;
-                if (from == to) continue;
-                alis[from].push_back({ to, cost });
-                alis[to].push_back({ from, cost });
-            }
-            dijkstra(pre_start_point);
-        }
-        // 여행 상품 생성
-        else if (creat_product == index) {
-            int tempid, revenue, dest;
-            cin >> tempid >> revenue >> dest;
-            sale_product[tempid] = { revenue, &dis[dest] };
-        }
-        // 여행 상품 취소
-        else if (cancel_product == index) {
-            int tempid;
-            cin >> tempid;
-            sale_product.erase(tempid);
-        }
-        // 최적의 여행 상품 판매
-        else if (opti_sales == index) {
-            if (!sale_product.empty()) {
-                int max_profit = 0;
-                int best_product_id = -1;
-
-                for (const auto& p : sale_product) {
-                    int profit = p.second.revenue - *(p.second.cost);
-                    if (profit >= max_profit) {
-                        max_profit = profit;
-                        best_product_id = p.first;
-                    }
-                }
-
-                if (best_product_id != -1) {
-                    cout << best_product_id << "\n";
-                    sale_product.erase(best_product_id);
-                }
-                else {
-                    cout << -1 << "\n";
-                }
-            }
-            else {
-                cout << -1 << "\n";
-            }
-        }
-        // 여행 상품의 출발지 변경
-        else if (change_start == index) {
-            int change;
-            cin >> change;
-            pre_start_point = change;
-            dijkstra(pre_start_point);
-        }
-    }
-    return 0;
+	int id, rev, dest; 
+	NODE node;
+	bool flag;
+	while (Q--) {
+		cin >> order;
+		switch (order) {
+			case 200:
+				cin >> id >> rev >> dest;
+				node = { rev, dest, land[dest] }; // 비용, 도착지, 거리
+				um.insert({ id, node });
+				if (rev >= land[dest]) {
+					pq.push({ rev - land[dest], id });
+				}
+				break;
+			case 300:
+				cin >> id;
+				um.erase(id);
+				break;
+			case 400:
+				if (um.empty()) {
+					cout << -1 << '\n';
+					continue;
+				}
+				flag = false;
+				while (!pq.empty()) {
+					int tmp_id = pq.top().second;
+					if (um.find(tmp_id) == um.end()) { // 삭제됨
+						pq.pop();
+						continue;
+					}
+					cout << tmp_id << '\n';
+					um.erase(tmp_id);
+					pq.pop();
+					flag = true;
+					break;
+				}
+				if(!flag) cout << -1 << '\n';
+				break;
+			case 500:
+				cin >> s;
+				dij(s);
+				
+				while (!pq.empty()) pq.pop();
+				for (auto it : um) {
+					it.second.distance = land[it.second.destination];
+					if (it.second.revenue >= land[it.second.destination]) {
+						pq.push({ it.second.revenue - land[it.second.destination], it.first });
+					}
+				}
+				break;
+		}
+	}
 }
