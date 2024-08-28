554bdb45fe27b09d78cd7cefc0cc60fe4a140e07
오정한 <129676250+JeongHanO@users.noreply.github.com>
2024-08-28 17:21:39 +0900

    Title: 고대 문명 유적 탐사, Time: 8ms, Memory: 0MB, Status: Passed - CodeTree

--- 240828/고대 문명 유적 탐사/ancient-ruin-exploration.cpp
+++ 240828/고대 문명 유적 탐사/ancient-ruin-exploration.cpp
@@ -1,196 +1,243 @@
 #include <iostream>
+#include <vector>
 #include <queue>
-#include <algorithm>
+#include <string.h>
 using namespace std;
+int k, m;
 
-int K, M;
-int board[5][5];
-queue<int> relicQ;
+int arr[5][5];
+int subcheck[5][5];
+int copy_arr[5][5];
+vector<int> photo;
 
-int dx[] = { 1, -1, 0, 0 };
-int dy[] = { 0, 0, 1, -1 };
+int dx[4] = { 0,0,1,-1 };
+int dy[4] = { 1,-1,0,0 };
 
-void input() {
-	cin >> K >> M;
-	for (int i = 0; i < 5; i++) {
-		for (int j = 0; j < 5; j++) {
-			cin >> board[i][j];
+int photoindex = 0;
+
+int remove(int i, int j, int target) {
+	queue<pair<int, int>> q;
+	q.push({ i,j });
+	int cnt = 1;
+	while (!q.empty()) {
+		auto temp = q.front();
+		int y = temp.first;
+		int x = temp.second;
+		q.pop();
+		for (int dir = 0; dir < 4; dir++) {
+			int nx = x + dx[dir];
+			int ny = y + dy[dir];
+			if (nx >= 5 || ny >= 5 || ny < 0 || nx < 0) continue;
+			if (copy_arr[ny][nx] == target && subcheck[ny][nx] == 0) {
+				subcheck[ny][nx] = 1;
+				q.push({ ny,nx });
+				cnt++;
+			}
 		}
 	}
-	for (int i = 0; i < M; i++) {
-		int a; cin >> a;
-		relicQ.push(a);
+
+	if (cnt >= 3) {
+		return cnt;
+	}
+	else {
+		return 0;
+	}
+
+}
+
+void realremove(int i, int j, int target) {
+
+	queue<pair<int, int>> q;
+	q.push({ i,j });
+	subcheck[i][j] = 1;
+	int cnt = 1;
+	while (!q.empty()) {
+		auto temp = q.front();
+		int y = temp.first;
+		int x = temp.second;
+		q.pop();
+		for (int dir = 0; dir < 4; dir++) {
+			int nx = x + dx[dir];
+			int ny = y + dy[dir];
+			if (nx >= 5 || ny >= 5 || ny < 0 || nx < 0) continue;
+			if (copy_arr[ny][nx] == target && subcheck[ny][nx] == 0) {
+				subcheck[ny][nx] = 1;
+				cnt++;
+				q.push({ ny,nx });
+			}
+		}
+	}
+	if (cnt >= 3) {
+		for (int ii = 0; ii < 5; ii++) {
+			for (int jj = 0; jj < 5; jj++) {
+				if (subcheck[ii][jj] == 1) {
+					copy_arr[ii][jj] = 0;
+				}
+			}
+		}
 	}
 }
 
-int cntRelic() {
-	int cntMax = 0;
-	bool visited[5][5] = { false };
-	for (int i = 0; i < 5; i++) {
-		for (int j = 0; j < 5; j++) {
-			if (!visited[i][j]) {
-				int cnt = 1;
-				visited[i][j] = true;
-				queue<pair<int, int>> q;
-				q.push({ i, j });
-				while (!q.empty()) {
-					int x = q.front().first;
-					int y = q.front().second;
-					q.pop();
-					for (int k = 0; k < 4; k++) {
-						int nx = x + dx[k];
-						int ny = y + dy[k];
-						if (nx < 0 || ny < 0 || nx == 5 || ny == 5 || visited[nx][ny] || board[nx][ny] != board[i][j]) continue;
-						visited[nx][ny] = true;
-						cnt++;
-						q.push({ nx, ny });
+void arr2copy() {
+	for (int ii = 0; ii < 5; ii++) {
+		for (int jj = 0; jj < 5; jj++) {
+			copy_arr[ii][jj] = arr[ii][jj];
+		}
+	}
+}
+
+void copy2arr() {
+	for (int ii = 0; ii < 5; ii++) {
+		for (int jj = 0; jj < 5; jj++) {
+			arr[ii][jj] = copy_arr[ii][jj];
+		}
+	}
+}
+
+void rotation(int i, int j, int k) {
+
+	//90도 일때,
+	if (k == 0) { // 90도
+		for (int ii = i - 1; ii < i + 2; ii++) {
+			for (int jj = j - 1; jj < j + 2; jj++) {
+				if (ii >= 0 && ii < 5 && jj >= 0 && jj < 5) {
+					int new_i = i + (jj - j);
+					int new_j = j - (ii - i);
+					if (new_i >= 0 && new_i < 5 && new_j >= 0 && new_j < 5) {
+						copy_arr[new_i][new_j] = arr[ii][jj];
 					}
 				}
-				if (cnt >= 3) {
-					cntMax += cnt;
-				}
 			}
 		}
 	}
-	return cntMax;
-}
-
-void turn(int x, int y) {
-	// x, y를 중심으로 3*3배열 시계 방향으로 90도 회전
-	int tmp_board[3][3];
-	for (int i = 0; i < 3; i++) {
-		for (int j = 0; j < 3; j++) {
-			tmp_board[j][2-i] = board[x+i-1][y+j-1];
-		}
-	}
-	
-	for (int i = 0; i < 3; i++) {
-		for (int j = 0; j < 3; j++) {
-			board[x + i - 1][y + j - 1] = tmp_board[i][j];
-		}
-	}
-}
-
-void turnBoard() {
-	// 회전 안하는 경우는 없음. 시계방향 90 180 270 중 하나
-	// 1. 유물 획득 최대화 2. 회전 각도가 가장 작음 3. 열이 작음, 행이 작음
-	int r, c, degree = -1, maxRelic = 0;
-	
-	// 유물을 최대로 획득 가능한 경우 탐색
-	for (int j = 1; j < 4; j++) {
-		for (int i = 1; i < 4; i++) {
-			int tmp_board[3][3];
-			// 회전 배열 저장
-			for (int x = 0; x <= 2 ; x++) {
-				for(int y = 0; y <= 2; y++) {
-					tmp_board[x][y] = board[i + x - 1][j + y - 1];
-				}
-			}
-			// 90도 회전을 세번. 한 번 회전할 때마다 최대 유물 수 확인.
-			for (int deg = 0; deg < 3; deg++) {
-				turn(i, j); // 90도 회전
-				int tmpCnt = cntRelic();
-				if (tmpCnt == 0) continue;
-				if (tmpCnt > maxRelic) {
-					r = i;
-					c = j;
-					degree = deg;
-					maxRelic = tmpCnt;
-				}
-				else if (tmpCnt == maxRelic) {
-					if (degree > deg) {
-						r = i;
-						c = j;
-						degree = deg;
+	//180도 일때,
+	else if (k == 1) { // 180도
+		for (int jj = j - 1; jj < j + 2; jj++) {
+			for (int ii = i - 1; ii < i + 2; ii++) {
+				if (ii >= 0 && ii < 5 && jj >= 0 && jj < 5) {
+					int new_i = i + (i - ii);
+					int new_j = j + (j - jj);
+					if (new_i >= 0 && new_i < 5 && new_j >= 0 && new_j < 5) {
+						copy_arr[new_i][new_j] = arr[ii][jj];
 					}
 				}
 			}
-
-			int d = 1;
-
-			// 회전 복원
-			for (int x = 0; x <= 2; x++) {
-				for (int y = 0; y <= 2; y++) {
-					board[i+x-1][j+y-1] = tmp_board[x][y];
-				}
-			}
 		}
 	}
-
-	for (int i = 0; i <= degree; i++) {
-		turn(r, c);
-	}
-}
-
-int getRelic() {
-	int cntGet = 0;
-	
-	bool visited[5][5] = { false };
-	for (int i = 0; i < 5; i++) {
-		for (int j = 0; j < 5; j++) {
-			if (visited[i][j]) continue;
-			visited[i][j] = true;
-			int relicNum = board[i][j];
-			int tmp_cnt = 1;
-			queue<pair<int, int>> q;
-			queue<pair<int, int>> dList;
-			q.push({ i,j });
-			dList.push({ i, j });
-			while (!q.empty()) {
-				int x = q.front().first;
-				int y = q.front().second;
-				q.pop();
-				for (int k = 0; k < 4; k++) {
-					int nx = x + dx[k];
-					int ny = y + dy[k];
-					if (nx < 0 || ny < 0 || nx == 5 || ny == 5 || visited[nx][ny] || board[nx][ny] != relicNum) continue;
-					visited[nx][ny] = true;
-					tmp_cnt++;
-					q.push({ nx, ny });
-					dList.push({ nx, ny });
+	//270도 일 때,
+	else { // 270도
+		for (int ii = i - 1; ii < i + 2; ii++) {
+			for (int jj = j - 1; jj < j + 2; jj++) {
+				if (ii >= 0 && ii < 5 && jj >= 0 && jj < 5) {
+					int new_i = i - (jj - j);
+					int new_j = j + (ii - i);
+					if (new_i >= 0 && new_i < 5 && new_j >= 0 && new_j < 5) {
+						copy_arr[new_i][new_j] = arr[ii][jj];
+					}
 				}
-			}
-			if (tmp_cnt >= 3) { 
-				cntGet += tmp_cnt;
-				int size = dList.size();
-				while (size--) {
-					board[dList.front().first][dList.front().second] = 0;
-					dList.pop();
-				}
-			}
-		}
-	}
-	
-	return cntGet;
-}
-
-void insertRelic() {
-	for (int j = 0; j < 5; j++) {
-		for (int i = 4; i >= 0; i--) {
-			if (board[i][j] == 0) {
-				board[i][j] = relicQ.front();
-				relicQ.pop();
 			}
 		}
 	}
 }
 
-void solve() {
-	for (int i = 0; i < K; i++) {
-		int sum = 0;
-		turnBoard();
-		int num = max(num, getRelic());
-		if (num == 0) return; // 수확할 수 있는 방법 없음
-		while (num != 0) { // 연쇄 획득 끝날 때까지
-			sum += num;
-			insertRelic();
-			num = getRelic();
-		}
-		cout << sum << ' ';
-	}
+void printarr() {
+
+	//cout << "############배열 출력###############\n";
+	//for(int i = 0; i < 5; i++) {
+	//    for (int j = 0; j < 5; j++) {
+	//        cout << copy_arr[i][j] << " ";
+	//    }
+	//    cout << "\n";
+	//}
 }
 
 int main() {
-	input();
-	solve();
+	cin >> k >> m;
+	for (int i = 0; i < 5; i++) {
+		for (int j = 0; j < 5; j++) {
+			cin >> arr[i][j];
+		}
+	}
+	for (int i = 0; i < m; i++) {
+		int temp;
+		cin >> temp;
+		photo.push_back(temp);
+	}
+
+	for (int test = 0; test < k; test++) {
+		int indexi = -1;
+		int indexj = -1;
+		int indexk = -1;
+		int max_sum = 0;
+		for (int kk = 0; kk < 3; kk++) {
+			for (int j = 1; j < 4; j++) {
+				for (int i = 1; i < 4; i++) {
+					arr2copy();
+					rotation(i, j, kk);
+					memset(subcheck, 0, sizeof(subcheck)); // subcheck 초기화
+					int tempsum = 0;
+					for (int ii = 0; ii < 5; ii++) {
+						for (int jj = 0; jj < 5; jj++) {
+							if (subcheck[ii][jj] == 0) {
+								subcheck[ii][jj] = 1;
+								tempsum += remove(ii, jj, copy_arr[ii][jj]);
+							}
+						}
+					}
+					if (tempsum > max_sum) {
+						indexi = i;
+						indexj = j;
+						indexk = kk;
+						max_sum = tempsum;
+					}
+				}
+			}
+		}
+
+		if (max_sum < 3) return 0;
+
+		arr2copy();
+		rotation(indexi, indexj, indexk);
+		for (int ii = 0; ii < 5; ii++) {
+			for (int jj = 0; jj < 5; jj++) {
+				memset(subcheck, 0, sizeof(subcheck));
+				realremove(ii, jj, copy_arr[ii][jj]);
+			}
+		}
+		printarr();
+		for (int jj = 0; jj < 5; jj++) {
+			for (int ii = 4; ii >= 0; ii--) {
+				if (copy_arr[ii][jj] == 0) {
+					copy_arr[ii][jj] = photo[photoindex]; // photoindex로 photo 배열 참조
+					photoindex++;
+				}
+			}
+		}
+		printarr();
+		while (1) {
+			int flag = 0;
+			for (int ii = 0; ii < 5; ii++) {
+				for (int jj = 0; jj < 5; jj++) {
+					memset(subcheck, 0, sizeof(subcheck));
+					realremove(ii, jj, copy_arr[ii][jj]);
+				}
+			}
+			printarr();
+			for (int jj = 0; jj < 5; jj++) {
+				for (int ii = 4; ii >= 0; ii--) {
+					if (copy_arr[ii][jj] == 0) {
+						flag = 1;
+						copy_arr[ii][jj] = photo[photoindex];
+						photoindex++;
+						max_sum++;
+					}
+				}
+			}
+			if (flag == 0) break;
+			printarr();
+		}
+		printarr();
+		copy2arr();
+		cout << max_sum << " ";
+	}
 }
